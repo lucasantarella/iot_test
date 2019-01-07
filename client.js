791f7bab8d0ef5fbb223dce04df5817764ee2cd0
Luca Santarella <luca.santarella@gmail.com>
2019-01-06 21:25:23 -0500

    Using API now

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 'use strict';
+const request = require('request');
 const autobahn = require('autobahn');
 const uuidv4 = require('uuid/v4');
 const gpio = require("gpio");
@@ -15,19 +16,58 @@ const gpio4 = gpio.export(4, {
 const device_uuid = process.env.BALENA_DEVICE_UUID || uuidv4();
 console.log('Device UUID is: ' + device_uuid);
 
+// WebSocket connection constants
 const ws_host = process.env.WS_HOST || '043f38db.ngrok.io';
 const ws_realm = process.env.WS_REALM || 'com.lucasantarella.iot';
 const ws_port = process.env.WS_PORT || '80';
 const ws_wss = process.env.WS_WSS || 'ws';
 
+// API connection constants
+const api_host = process.env.API_HOST || '043f38db.ngrok.io';
+const api_port = process.env.API_PORT || '80';
+const api_https = process.env.API_HTTPS || 'https';
+
+const api_uri = api_https + '://' + api_host + ':' + api_port + '/';
+
 let connection = new autobahn.Connection({
-  url: ws_wss + '://' + ws_host + ':' + ws_port + '/',
+  url: ws_wss + '://' + ws_host + ':' + ws_port,
   realm: ws_realm,
   tlsConfiguration: {}
 });
 
+function registerWithApi () {
+  request.post(api_uri + '/devices', {
+    json: {
+      "id": device_uuid,
+      "type": "action.devices.types.LIGHT",
+      "traits": [
+        "action.devices.traits.OnOff",
+      ],
+      "name": {
+        "defaultNames": ["LED Light"],
+        "name": "ledlight",
+        "nicknames": ["reading lamp"]
+      },
+      "willReportState": false,
+      "attributes": {},
+      "deviceInfo": {
+        "manufacturer": "SANTARELLA",
+        "model": "LED-100",
+        "hwVersion": "1.0.0",
+        "swVersion": "1.0.0"
+      },
+      "customData": {}
+    }
+  }, (error, res, body) => {
+    if (error) {
+      console.error(error);
+      return
+    }
+    console.log(body);
+  })
+}
+
 connection.onopen = function (session) {
-  // 1) subscribe to a topic
   function onevent(args) {
     console.log("Event:", args[0]);
     gpio4.set(!(gpio4.value), function () {
